5f16d355dc97f598a65dea8ae244c9c425d5874b
Santiago Arboleda <arboledamartinezs222@gmail.com>
2024-04-10 23:06:04 -0500

    first commit

---
+++ search.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.1.1.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script  src="js/jquery.ui.totop.js"></script>
+
+    <script src="js/jquery.equalheights.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    
+  <!--[if lt IE 8]>
+      <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+  <![endif]-->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header class="contpages2">
+<div class="headtop"></div>
+    <div class="container">
+        <h1 class="brand brand_"><a href="index.html"><img alt="i love cats" src="img/logo.png"></a></h1>       
+        <div class="navbar navbar_ clearfix extra">
+          <div class="nav-collapse nav-collapse_  collapse">
+            <ul class="nav sf-menu">
+              <li><a class="margin1" href="index.html">home</a><span class="bgcloud"></span></li>
+              <li><a class="margin2" href="index-1.html">about me</a><span class="bgcloud"></span></li>
+              <li class="right1"><a class="margin3" href="index-2.html">journal</a><span class="bgcloud"></span>
+                <ul>
+                  <li><a href="#">may 2013</a></li>
+                  <li><a href="#">april 2013</a></li>
+                  <li><a href="#">march 2013</a>
+                    <ul>
+                        <li><a href="#">20 march</a></li>   
+                        <li><a href="#">15 march</a></li>   
+                        <li><a href="#">10 march</a></li>                                  
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a class="margin4" href="index-3.html">blog</a><span class="bgcloud"></span></li>
+              <li class="last"><a class="margin5" href="index-4.html">contacts</a><span class="bgcloud"></span></li>
+            </ul>
+          </div>
+        </div> 
+    </div>  
+</header> 
+<!--==============================content=================================-->
+<section id="content">
+    <div class="row_13">
+        <div class="bgcolor3">
+          <div class="container">
+            <div class="row">
+              <div class="span12">
+               <h2 class="h2-pad">Search result:</h2>
+                <div id="search-results"></div>
+               </div>
+               </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--==============================footer=================================-->
+<footer>
+    <div class="row_4">
+        <div class="bgcolorfooter">
+            <div class="container">
+                <div class="row">    
+                    <div class="span12">
+                        <div class="">
+                            <a href="index.html"><img class="smallogo" alt="Simpatico" src="img/smalllogo.png"></a>
+                            <div class="privacytxt"><span class="txt-1">&copy; 2013 </span><a class="txt-1" href="index-5.html">Privacy policy</a>&nbsp;</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
